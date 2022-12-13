12fbbd9ec5ba63b1d36e6d5ff3de175a006d0a41
black-cap-corp <blackcapcorp@gmail.com>
2022-12-13 21:01:32 +0530

    Testing message

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -13,7 +13,7 @@ const options = {
   // you can also just use 'bottom center'
   position: positions.TOP_RIGHT,
   timeout: 3000,
-  offset: "30px",
+  offset: "32px",
   // you can also just use 'scale'
   transition: transitions.SCALE,
 };
