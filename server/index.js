c415d5e59c0e56e8a793ecf1591a758cd8e23962
black-cap-corp <blackcapcorp@gmail.com>
2022-12-16 09:35:41 +0530

    changed the default server port number

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = require("./app");
 const server = http.createServer(app);
 
 const { API_PORT } = process.env;
-const port = API_PORT || 8000;
+const port = API_PORT || 5000;
 
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
