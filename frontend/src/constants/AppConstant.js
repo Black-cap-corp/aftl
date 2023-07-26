a544a2ac39b4374ca8a03237893c882ca25f0730
black-cap-corp <blackcapcorp@gmail.com>
2023-07-26 18:50:19 +0530

    fixed a bug

--- frontend/src/constants/AppConstant.js
+++ frontend/src/constants/AppConstant.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:8000";
-// export const BASE_URL =
-//   "http://ec2-65-0-18-121.ap-south-1.compute.amazonaws.com";
+//export const BASE_URL = "http://localhost:8000";
+export const BASE_URL =
+  "http://ec2-65-0-18-121.ap-south-1.compute.amazonaws.com";
