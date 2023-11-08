412d88cace5eaa84cb94da86ab93a0274b69187f
black-cap-corp <blackcapcorp@gmail.com>
2023-11-08 21:30:04 +0530

    reset app url's

--- mobile/app.const.js
+++ mobile/app.const.js
@@ -1,3 +1,3 @@
-export const APP_BASE_URL = 'http://10.0.2.2:8000';
-// export const APP_BASE_URL =
-//   'http://ec2-65-0-18-121.ap-south-1.compute.amazonaws.com';
+//export const APP_BASE_URL = 'http://10.0.2.2:8000';
+export const APP_BASE_URL =
+  'http://ec2-65-0-18-121.ap-south-1.compute.amazonaws.com';
